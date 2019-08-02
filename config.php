743a54c1644b658a33540809b0d4becdf4ea7173
Israel padigitale <igruiz91@gmail.com>
2019-08-02 12:16:05 +0200

    paypal añadido

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    require 'paypal/autoload.php';
+
+    $apiContext = new \Paypal\Rest\ApiContext(
+        new \PayPal\Auth\OAuthTokenCredential('AQBDgdyuxA_dH9a4n5THRs8NMybw7uPAvTKnDiK-ioIK-0Szc_LuqYsIIeuSo2epSpi3iJuQd6C5mtNH', 'EC-51kBsjjQMpj8UUnFYf1a1k3ERokLNp5QBVH3AwY4t25jcZ7YUcq_Vj2phneWd_95Xs-tLCiWTMJPz'
+        )
+    );
+
+?>
