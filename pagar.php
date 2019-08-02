89dd99aaca127032643097c65f84b68153b46e2e
Israel padigitale <igruiz91@gmail.com>
2019-08-02 17:29:56 +0200

    nuevo sdk paypal config y pagar

--- pagar.php
+++ pagar.php
@@ -4,13 +4,24 @@ if(!isset($_POST['producto'], $_POST['precio'])){
     exit("Hubo un error");
 }
 
+use PayPal\Api\Payer;
+use PayPal\Api\Item;
+
 require 'config.php';
 
 $producto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
 $precio = (float) $precio;
-var_dump($precio);
 $envio = 0;
 $total = $precio + $envio;
 
-?>
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+$articulo = new Item();
+$articulo->setName($producto)
+         ->setCurrency('USD')
+         ->setQuantity(1)
+         ->setPrice($precio);
+
+echo $articulo->getPrice();
