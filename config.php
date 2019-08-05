0a8a0f925c6e040431a8661dfdbd9db44082c64a
Israel padigitale <igruiz91@gmail.com>
2019-08-05 11:17:57 +0200

    finalizado pagos probados

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 
     require 'paypal/autoload.php';
 
+    define('URL_SITIO', 'http://localhost:8080/paypal');
+
     $apiContext = new \PayPal\Rest\ApiContext(
         new \PayPal\Auth\OAuthTokenCredential(
             'AQBDgdyuxA_dH9a4n5THRs8NMybw7uPAvTKnDiK-ioIK-0Szc_LuqYsIIeuSo2epSpi3iJuQd6C5mtNH','EC-51kBsjjQMpj8UUnFYf1a1k3ERokLNp5QBVH3AwY4t25jcZ7YUcq_Vj2phneWd_95Xs-tLCiWTMJPz' 
--- pagar.php
+++ pagar.php
@@ -6,13 +6,19 @@ if(!isset($_POST['producto'], $_POST['precio'])){
 
 use PayPal\Api\Payer;
 use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Details;
+use PayPal\Api\Amount;
+use PayPal\Api\Transaction;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Payment;
 
 require 'config.php';
 
 $producto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
 $precio = (float) $precio;
-$envio = 0;
+$envio = 5;
 $total = $precio + $envio;
 
 $compra = new Payer();
@@ -24,4 +30,43 @@ $articulo->setName($producto)
          ->setQuantity(1)
          ->setPrice($precio);
 
-echo $articulo->getPrice();
+$listaArticulos = new ItemList();
+$listaArticulos->setItems(array($articulo));
+
+$detalles = new Details();
+$detalles->setShipping($envio)
+        ->setSubtotal($precio);
+
+$cantidad = new Amount();
+$cantidad->setCurrency('USD')
+        ->setTotal($total)
+        ->setDetails($detalles);
+
+$transaccion = new Transaction();
+$transaccion->setAmount($cantidad)
+            ->setItemList($listaArticulos)
+            ->setDescription('Pago Paypal')
+            ->setInvoiceNumber(uniqid());
+
+$redireccionar = new RedirectUrls();
+$redireccionar->setReturnUrl(URL_SITIO . "/pago_finalizado.php?exito=true")
+            ->setCancelUrl(URL_SITIO . "/pago_finalizado.php?exito=false");
+
+$pago = new Payment();
+$pago->setIntent("sale")
+    ->setPayer($compra)
+    ->setRedirectUrls($redireccionar)
+    ->setTransactions(array($transaccion));
+
+try{
+    $pago->create($apiContext);
+}catch(\PayPal\Exception\PayPalConnectionException $pce){
+    echo"<pre>";
+    print_r(json_decode($pce->getData()));
+    exit;
+    echo"</pre>";
+}
+
+$aprobado = $pago->getApprovalLink();
+
+header("Location:{$aprobado}");
